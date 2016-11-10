9f8235395f7162ac4b3b1ccb46af8e7ce7fc5273
skliba <akljaicka@gmail.com>
2016-11-10 11:59:06 +0100

    Login koliko toliko zavrsen

--- app/src/main/java/foi/hr/firewatchapp/LoginActivity.java
+++ app/src/main/java/foi/hr/firewatchapp/LoginActivity.java
@@ -1,14 +1,30 @@
 package foi.hr.firewatchapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        
+
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnRegister = (Button) findViewById(R.id.btn_register);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+            }
+        });
+
     }
 }
