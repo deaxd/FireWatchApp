6dee72c534a527a3b0316bbbb08181341343c582
dsafaric1 <dsafaric@foi.hr>
2016-11-18 07:43:49 +0100

    Dodana lokalna baza
    Dodani List atributi.

--- database/src/main/java/hr/foi/air/database/database/entities/Intervention.java
+++ database/src/main/java/hr/foi/air/database/database/entities/Intervention.java
@@ -2,10 +2,15 @@ package hr.foi.air.database.database.entities;
 
 import com.raizlabs.android.dbflow.annotation.Column;
 import com.raizlabs.android.dbflow.annotation.ForeignKey;
+import com.raizlabs.android.dbflow.annotation.OneToMany;
 import com.raizlabs.android.dbflow.annotation.PrimaryKey;
 import com.raizlabs.android.dbflow.annotation.Table;
 import com.raizlabs.android.dbflow.annotation.Unique;
+import com.raizlabs.android.dbflow.sql.language.Join;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
 import com.raizlabs.android.dbflow.structure.BaseModel;
+
+import java.util.List;
 
 import hr.foi.air.database.database.FireWatchDB;
 
@@ -29,10 +34,15 @@ public class Intervention extends BaseModel {
     @Column @ForeignKey(tableClass = Fireman.class)
     Fireman fireman;
 
+    List<Equipment> equipmentsOnIntervention;
+    List<Vehicle> vehiclesOnIntervention;
+    List<Fireman> firemensOnIntervention;
+
     public Intervention() {
     }
 
-    public Intervention(int interventionId, int alertNumber, String kindOfIntervention, String address, String initialTIme, int duration, String description, int longitude, int latitude, Fireman fireman) {
+    public Intervention(int interventionId, int alertNumber, String kindOfIntervention, String address,
+                        String initialTIme, int duration, String description, int longitude, int latitude, Fireman fireman) {
         this.interventionId = interventionId;
         this.alertNumber = alertNumber;
         this.kindOfIntervention = kindOfIntervention;
@@ -123,5 +133,33 @@ public class Intervention extends BaseModel {
 
     public void setFireman(Fireman fireman) {
         this.fireman = fireman;
+    }
+
+    @OneToMany(methods = {OneToMany.Method.ALL}, variableName = "equipmentsOnIntervention")
+    public List<Equipment> getEquipmentsOnIntervention(){
+                equipmentsOnIntervention = SQLite.select().from(Equipment.class).join(EquipmentOnIntervention.class, Join.JoinType.INNER)
+                .on(Equipment_Table.equipmentId.withTable().eq(EquipmentOnIntervention_Table.equipment_equipmentId.withTable()))
+                .join(Intervention.class, Join.JoinType.INNER).on(EquipmentOnIntervention_Table.intervention_interventionId.withTable()
+                .eq(interventionId)).queryList();
+    return equipmentsOnIntervention;
+    }
+
+
+    @OneToMany(methods = {OneToMany.Method.ALL}, variableName = "vehiclesOnIntervention")
+    public List<Vehicle> getVehiclesOnIntervention(){
+            vehiclesOnIntervention = SQLite.select().from(Vehicle.class).join(VehicleOnIntervention.class, Join.JoinType.INNER)
+            .on(Vehicle_Table.vehicleId.withTable().eq(VehicleOnIntervention_Table.vehicle_vehicleId.withTable()))
+            .join(Intervention.class, Join.JoinType.INNER).on(VehicleOnIntervention_Table.intervention_interventionId.withTable()
+            .eq(interventionId)).queryList();
+    return vehiclesOnIntervention;
+    }
+
+    @OneToMany(methods = {OneToMany.Method.ALL}, variableName = "firemansOnIntervention")
+    public List<Fireman> getFiremensOnIntervention(){
+        firemensOnIntervention = SQLite.select().from(Fireman.class).join(FiremanOnIntervention.class, Join.JoinType.INNER)
+        .on(Fireman_Table.oib.withTable().eq(FiremanOnIntervention_Table.fireman_oib.withTable()))
+        .join(Intervention.class, Join.JoinType.INNER).on(FiremanOnIntervention_Table.intervention_interventionId.withTable()
+        .eq(interventionId)).queryList();
+    return firemensOnIntervention;
     }
 }
--- database/src/main/java/hr/foi/air/database/database/entities/Organization.java
+++ database/src/main/java/hr/foi/air/database/database/entities/Organization.java
@@ -1,9 +1,14 @@
 package hr.foi.air.database.database.entities;
 
 import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.OneToMany;
 import com.raizlabs.android.dbflow.annotation.PrimaryKey;
 import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+import com.raizlabs.android.dbflow.sql.language.Where;
 import com.raizlabs.android.dbflow.structure.BaseModel;
+
+import java.util.List;
 
 import hr.foi.air.database.database.FireWatchDB;
 
@@ -19,6 +24,12 @@ public class Organization extends BaseModel {
     @Column String name;
     @Column String shortName;
     @Column String address;
+
+
+    List<Fireman> members;
+    List<Fireman> myLiuetenants;
+    List<Vehicle> myVehicles;
+    List<Equipment> myEquipments;
 
     public Organization() {
     }
@@ -61,4 +72,29 @@ public class Organization extends BaseModel {
     public void setAddress(String address) {
         this.address = address;
     }
+
+    @OneToMany(methods = {OneToMany.Method.ALL}, variableName = "members")
+    public List<Fireman> getMembers(){
+        members = SQLite.select().from(Fireman.class).where(Fireman_Table.organization_organizationId.eq(organizationId)).queryList();
+        return members;
+    }
+
+    @OneToMany(methods = {OneToMany.Method.ALL}, variableName = "myLiuetenants")
+    public List<Fireman> getMyLiuetenants(){
+        myLiuetenants = SQLite.select().from(Fireman.class).where(Fireman_Table.organization_organizationId.eq(organizationId)).and(Fireman_Table.lieutenant.eq(true)).queryList();
+        return myLiuetenants;
+    }
+
+    @OneToMany(methods = {OneToMany.Method.ALL},variableName = "myVehicles")
+    public List<Vehicle> getMyVehicles(){
+        myVehicles = SQLite.select().from(Vehicle.class).where(Vehicle_Table.organization_organizationId.eq(organizationId)).queryList();
+        return myVehicles;
+    }
+
+    @OneToMany(methods = {OneToMany.Method.ALL}, variableName = "myEquipments")
+    public List<Equipment> getMyEquipments(){
+        myEquipments = SQLite.select().from(Equipment.class).where(Equipment_Table.organization_organizationId.eq(organizationId)).queryList();
+        return myEquipments;
+    }
+
 }
