ab397f7428c5752e92be06eef0c21dbea9670720
Deaxd <akljaicka@gmail.com>
2017-01-07 20:20:38 +0100

    Dodan inicijalni fragment na kojem se pokazuje lista clanova

--- members/src/main/java/foi/hr/members/MembersActivity.java
+++ members/src/main/java/foi/hr/members/MembersActivity.java
@@ -1,13 +1,65 @@
 package foi.hr.members;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import foi.hr.members.fragments.MemberDetailsFragment;
+import foi.hr.members.fragments.MembersFragment;
+import foi.hr.members.fragments.NewMemberFragment;
 
 public class MembersActivity extends AppCompatActivity {
+
+    private FrameLayout fragmentContainer;
+
+    private FloatingActionButton fab;
+
+    private List<Fragment> fragmentList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_members);
+
+        addFragmentsToList();
+
+        fragmentContainer = (FrameLayout) findViewById(R.id.fragment_container);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onFabClicked();
+            }
+        });
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.fragment_container, fragmentList.get(0));
+        ft.commit();
     }
+
+    private void onFabClicked() {
+        
+    }
+
+    private void addFragmentsToList() {
+        MembersFragment membersFragment = new MembersFragment();
+        NewMemberFragment newMemberFragment = new NewMemberFragment();
+        MemberDetailsFragment memberDetailsFragment = new MemberDetailsFragment();
+
+        fragmentList.add(membersFragment);
+        fragmentList.add(newMemberFragment);
+        fragmentList.add(memberDetailsFragment);
+    }
+
+
 }
